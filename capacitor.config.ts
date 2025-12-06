b1fccd25980a2d7ce7763d3eae51f7cfdccc0923
Mabel Asogwa <mabel.asogwa@gmail.com>
2025-12-06 00:56:13 +0000

    Initial commit — project setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'G0047287',
+  webDir: 'www'
+};
+
+export default config;
